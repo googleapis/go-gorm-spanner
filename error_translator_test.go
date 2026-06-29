f6f19744fc9dda44c8458626b1505f57b26276ec
Gage McGuire <72943845+Gage-McGuire@users.noreply.github.com>
2026-06-29 17:04:25 -0500

    feat: translate Spanner AlreadyExists errors to gorm.ErrDuplicatedKey

---
+++ error_translator_test.go
@@ -0,0 +1,50 @@
+package gorm
+
+import (
+	"errors"
+	"testing"
+
+	"cloud.google.com/go/spanner"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"gorm.io/gorm"
+)
+
+var testErrors = map[codes.Code]error{
+	codes.AlreadyExists:   spanner.ToSpannerError(status.Errorf(codes.AlreadyExists, "already exists")),
+	codes.InvalidArgument: spanner.ToSpannerError(status.Errorf(codes.InvalidArgument, "invalid argument")),
+	codes.Unknown:         errors.New("random error"),
+}
+
+func TestErrorTranslator(t *testing.T) {
+	tests := []struct {
+		name string
+		err  error
+		want error
+	}{
+		{
+			name: "AlreadyExists returns gorm.ErrDuplicatedKey",
+			err:  testErrors[codes.AlreadyExists],
+			want: gorm.ErrDuplicatedKey,
+		},
+		{
+			name: "InvalidArgument returns original error",
+			err:  testErrors[codes.InvalidArgument],
+			want: testErrors[codes.InvalidArgument],
+		},
+		{
+			name: "Random error returns original error",
+			err:  testErrors[codes.Unknown],
+			want: testErrors[codes.Unknown],
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			dialector := Dialector{}
+			got := dialector.Translate(tt.err)
+			if got != tt.want {
+				t.Errorf("translate error mismatch:\n Got: %v\nWant: %v", got, tt.want)
+			}
+		})
+	}
+}
